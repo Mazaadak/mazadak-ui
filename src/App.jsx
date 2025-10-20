b7c82ace83b6986d8e6bbe8fa8017c0bbae12712
Hassan Ali <162273873+HassanIsmail16@users.noreply.github.com>
2025-10-20 16:45:51 +0300

    chore: add shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
+    // testing
     <div className="flex h-screen items-center justify-center">
-      <p className="font-bold text-9xl">Mazadak</p>
+      <div className="flex-direction flex-col items-center gap-8">
+        <p className="font-bold text-9xl">Mazadak</p>
+        <Button className="py-6 px-60 my-4">Click me</Button>
+      </div>
     </div>
   );
 }
